049f85eac60280095863075a241824e2ffa6141e
NicolasDelmondes <108539498+NicolasDelmondes@users.noreply.github.com>
2025-05-06 23:42:58 -0400

    Update App.java

--- Analisador-LexPascal/src/App.java
+++ Analisador-LexPascal/src/App.java
@@ -3,7 +3,7 @@ import Mecanismo.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Executor exec = new Executor();
-        exec.CarregarArquivo("Analisador-LexPascal/src/Pascal/Saudacao.pas");
+        exec.CarregarArquivo("src/Pascal/Saudacao.pas");
         exec.ProcessarBufferPrimario();
         exec.ImprimirBufferPrimario();
         exec.ProcessarBufferSecundario();
